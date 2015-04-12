c55018e456f13bb687857a90a6dce6bd82f47d7c
Matthew Gamble <git@matthewgamble.net>
2015-04-12 21:22:57 +1000

    Actually understand python-setuptools

--- setup.py
+++ setup.py
@@ -1,15 +1,15 @@
 #!/usr/bin/env python
 
-from setuptools import setup
+from setuptools import setup, find_packages
 
 setup(name="idiotscript",
-      version="1.1.0",
+      version="1.1.1",
       description="An easier, less powerful alternative to regular expressions",
       author="Matthew Gamble",
       author_email="git@matthewgamble.net",
       url="https://github.com/djmattyg007/IdiotScript",
-      download_url="https://github.com/djmattyg007/IdiotScript/archive/1.1.0.zip",
-      packages=['idiotscript'],
+      download_url="https://github.com/djmattyg007/IdiotScript/archive/1.1.1.zip",
+      packages=find_packages(),
       scripts=['bin/idiotscript.py'],
       package_data={'idiotscript': ['README.rst']},
       data_files=[('share/doc/python-idiotscript', ['README.txt', 'LICENSE.txt'])],
