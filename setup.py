cb389e7eb86d8bc54baf6e5ffbf3e39510b448e2
Matthew Gamble <git@matthewgamble.net>
2015-04-12 21:11:40 +1000

    Bump minor version number

--- setup.py
+++ setup.py
@@ -3,12 +3,12 @@
 from setuptools import setup
 
 setup(name="idiotscript",
-      version="1.0.1",
+      version="1.1.0",
       description="An easier, less powerful alternative to regular expressions",
       author="Matthew Gamble",
       author_email="git@matthewgamble.net",
       url="https://github.com/djmattyg007/IdiotScript",
-      download_url="https://github.com/djmattyg007/IdiotScript/archive/1.0.1.zip",
+      download_url="https://github.com/djmattyg007/IdiotScript/archive/1.1.0.zip",
       packages=['idiotscript'],
       scripts=['bin/idiotscript.py'],
       package_data={'idiotscript': ['README.rst']},
