0e8d3ba1d98a86cca0fd01bdb0d5f81c03c2d0df
Matthew Gamble <git@matthewgamble.net>
2022-01-23 18:20:13 +1100

    Improve setup.py

--- setup.py
+++ setup.py
@@ -2,18 +2,20 @@
 
 from setuptools import setup, find_packages
 
-setup(name="idiotscript",
-      version="1.1.1",
-      description="An easier, less powerful alternative to regular expressions",
-      author="Matthew Gamble",
-      author_email="git@matthewgamble.net",
-      url="https://github.com/djmattyg007/IdiotScript",
-      download_url="https://github.com/djmattyg007/IdiotScript/archive/1.1.1.zip",
-      packages=find_packages(),
-      scripts=['bin/idiotscript.py'],
-      package_data={'idiotscript': ['README.rst']},
-      data_files=[('share/doc/python-idiotscript', ['README.txt', 'LICENSE.txt'])],
-      license="UNLICENSE",
-      install_requires=[]
-      )
+version = "1.1.1"
 
+setup(
+    name="idiotscript",
+    version=version,
+    description="An easier, less powerful alternative to regular expressions",
+    author="Matthew Gamble",
+    author_email="git@matthewgamble.net",
+    url="https://github.com/djmattyg007/IdiotScript",
+    download_url=f"https://github.com/djmattyg007/IdiotScript/archive/{version}.zip",
+    packages=find_packages(),
+    scripts=['bin/idiotscript.py'],
+    package_data={'idiotscript': ['README.txt']},
+    data_files=[('share/doc/python-idiotscript', ['README.txt', 'LICENSE.txt'])],
+    license="UNLICENSE",
+    install_requires=[],
+)
